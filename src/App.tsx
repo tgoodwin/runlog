8efd0abc3c6f8f8352c434e2df2b7f8b31671543
Tim Goodwin <timg.goodwin@gmail.com>
2023-07-22 21:38:22 -0700

    edit run

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { createResource, createSignal, For, Show, batch } from "solid-js";
+import { createResource, createSignal, For, Show, batch, type JSX } from "solid-js";
 
 import { formatDateStr, createRunWeek, groupByWeek } from "./util";
-import { Run, RunWeek, Day } from './index';
+import { Run, RunWeek, Day } from "./index";
 import { createClient } from "@supabase/supabase-js";
 
 const ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
@@ -19,27 +19,25 @@ export async function getRuns() {
 }
 
 export async function addRun(r: Run) {
-  const { name, planned_miles, actual_miles, date } = r;
   const { error } = await supabase
     .from("runs")
-    .insert({ name, planned_miles, actual_miles, date });
+    .insert(r);
   return error;
 }
 
 export async function updateRun(r: Run) {
+  console.log(r);
   const { error } = await supabase
     .from("runs")
     .update({
       name: r.name,
       planned_miles: r.planned_miles,
       actual_miles: r.actual_miles,
-      date: r.date,
+      notes: r.notes,
     })
     .eq("id", r.id);
   return error;
 }
-
-
 
 function sum(runs: Run[], get: (r: Run) => number) {
   return runs.reduce((acc, r) => acc + get(r), 0);
@@ -47,41 +45,34 @@ function sum(runs: Run[], get: (r: Run) => number) {
 
 function sumWeek(week: RunWeek, get: (r: Run) => number) {
   return Object.values(week).reduce((acc, runs) => acc + sum(runs, get), 0);
-};
+}
 
 interface Props {
   run?: Run;
   submit: (r: Run) => void;
+  close: () => void;
 }
 
-function EditRun(props: Props) {
-  const [ showModal, setShowModal ] = createSignal(false);
-  const [ runTitle, setRunTitle ] = createSignal('');
-  const [ plannedMiles, setPlannedMiles ] = createSignal('');
-  const [ actualMiles, setActualMiles ] = createSignal('');
-  const [ runDate, setRunDate ] = createSignal('');
-  const [ runNotes, setRunNotes ] = createSignal('');
-
-  const clear = () => {
-    setRunTitle('');
-    setPlannedMiles('');
-    setActualMiles('');
-    setRunDate('');
-    setRunNotes('');
-  };
+function ModalForm(props: Props) {
+  const [ runTitle, setRunTitle ] = createSignal("");
+  const [ plannedMiles, setPlannedMiles ] = createSignal("");
+  const [ actualMiles, setActualMiles ] = createSignal("");
+  const [ runDate, setRunDate ] = createSignal("");
+  const [ runNotes, setRunNotes ] = createSignal("");
 
   if (props.run) {
     setRunTitle(props.run.name);
     setPlannedMiles(props.run.planned_miles.toString());
-    setActualMiles(props.run.actual_miles.toString());
+    setActualMiles(props.run.actual_miles?.toString());
     setRunDate(props.run.date);
-    setRunNotes(props.run.notes || '');
+    setRunNotes(props.run.notes || "");
   }
 
   const submitRun = (e: Event) => {
     e.preventDefault();
     batch(() => {
       const r: Run = {
+        id: props.run?.id,
         name: runTitle(),
         date: !!runDate() ? runDate() : new Date().toISOString(),
         planned_miles: parseFloat(plannedMiles()),
@@ -89,43 +80,96 @@ function EditRun(props: Props) {
         notes: runNotes(),
       };
       props.submit(r);
-      clear();
     });
   };
 
-  const cancel = () => {
-    setShowModal(false);
-    clear();
-  };
-
   return (
-    <div class="add-run">
-      <Show when={showModal()}>
+    <div class="add-run-container">
+      <div class="add-run">
         <form>
-          <input type="text" placeholder="run name" value={runTitle()} onInput={(e) => setRunTitle(e.currentTarget.value)} />
-          <input type="date" placeholder="run date" value={runDate()} onInput={(e) => setRunDate((e.currentTarget.value))} />
-          <input type="number" placeholder="planned miles" required value={plannedMiles()} onInput={(e) => setPlannedMiles((e.currentTarget.value))} />
-          <input type="number" placeholder="actual miles" value={actualMiles()} onInput={(e) => setActualMiles((e.currentTarget.value))} />
-          <input type="text" placeholder="notes" value={runNotes()} onInput={(e) => setRunNotes((e.currentTarget.value))} />
-          <button type="button" onClick={submitRun}>submit</button>
-          <button type="button" onClick={cancel}>cancel</button>
+          <input
+            type="text"
+            placeholder="run name"
+            value={runTitle()}
+            onInput={(e) => setRunTitle(e.currentTarget.value)}
+          />
+          <input
+            type="date"
+            placeholder="run date"
+            value={runDate()}
+            onInput={(e) => setRunDate(e.currentTarget.value)}
+          />
+          <input
+            type="number"
+            placeholder="planned miles"
+            required
+            value={plannedMiles()}
+            onInput={(e) => setPlannedMiles(e.currentTarget.value)}
+          />
+          <input
+            type="number"
+            placeholder="actual miles"
+            value={actualMiles()}
+            onInput={(e) => setActualMiles(e.currentTarget.value)}
+          />
+          <textarea
+            placeholder="notes"
+            cols={70}
+            value={runNotes()}
+            onInput={(e) => setRunNotes(e.currentTarget.value)}
+          />
+          <div>
+            <button type="button" onClick={submitRun}>
+              submit
+            </button>
+            <button type="button" onClick={props.close}>
+              cancel
+            </button>
+          </div>
         </form>
-      </Show>
-      <button type="button" onClick={() => setShowModal(true)}>add run</button>
+      </div>
     </div>
   );
 }
 
+type ModalProps = {
+  run?: Run;
+  submit: (r: Run) => void;
+  children: (open: () => void) => JSX.Element;
+};
+
+function RunModal(props: ModalProps) {
+  const [ showModal, setShowModal ] = createSignal(false);
+  const onSubmit = (r: Run) => {
+    props.submit(r);
+    setShowModal(false);
+  };
+  return (
+    <div>
+      <Show when={showModal()}>
+        <ModalForm run={props.run} submit={onSubmit} close={() => setShowModal(false)} />
+      </Show>
+      {props.children(() => setShowModal(true))}
+    </div>
+  );
+}
+
+
 function RunView(props: { run: Run; }) {
   return (
     <div class="run">
-      <div>{props.run.name || 'run'}</div>
+      <div>{props.run.name || "run"}</div>
       <div>{props.run.date}</div>
-      <div>{props.run.planned_miles} mi / {props.run.actual_miles || '--'} mi</div>
+      <div>
+        {props.run.planned_miles} mi / {props.run.actual_miles || "--"} mi
+      </div>
       <div>{props.run.notes}</div>
+      <RunModal run={props.run} submit={updateRun}>
+        {(showModal) => <div onClick={showModal}>[edit]</div>}
+      </RunModal>
     </div>
   );
-};
+}
 
 function WeekView(data: RunWeek) {
   const days = Object.keys(data);
@@ -136,24 +180,31 @@ function WeekView(data: RunWeek) {
           {(day) => {
             const runs: Run[] = data[ day as Day ];
             return (
-              <div>
+              <div class="week-item">
                 <div class="week-item">{day}</div>
-                <div class="week-item">
-                  <For each={runs}>
-                    {(r) => <RunView run={r} />}
-                  </For>
-                </div>
+                {runs.length ? (
+                  <div>
+                    <For each={runs}>{(r) => <RunView run={r} />}</For>
+                  </div>
+                ) : (
+                  <div class="run">
+                    <RunModal submit={addRun}>
+                      {(showModal) => <div onClick={showModal}>+</div>}
+                    </RunModal>
+                  </div>
+                )}
               </div>
             );
           }}
         </For>
       </div>
       <div class="week-summary">
-        total planned: {sumWeek(data, r => r.planned_miles)} mi | total completed: {sumWeek(data, r => r.actual_miles)} mi
+        total planned: {sumWeek(data, (r) => r.planned_miles)} mi | total
+        completed: {sumWeek(data, (r) => r.actual_miles)} mi
       </div>
     </div>
   );
-}
+};
 
 function App() {
   const [ runs, { refetch } ] = createResource<Run[]>(getRuns);
@@ -161,12 +212,13 @@ function App() {
 
   return (
     <div>
+      <div class="week-container">
       <h1>runlog</h1>
-      <EditRun submit={(r: Run) => addRun(r).then(refetch)} />
-      <Show
-        when={!runs.loading}
-        fallback={<div>loading...</div>}
-      >
+      <RunModal submit={(r) => addRun(r).then(refetch)}>
+        {(showModal) => <h1 class="open-modal" onClick={showModal}>(+)</h1>}
+      </RunModal>
+      </div>
+      <Show when={!runs.loading} fallback={<div>loading...</div>}>
         <div class="run-container">
           <For each={Object.entries(byWeek())}>
             {([ week, weekRuns ]) => {
